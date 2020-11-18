a1aa7c06a8f310f9a64f38178fe82506939f2f87
Courtney828 <courtneyserongwa@gmail.com>
2020-11-18 11:44:59 +0200

    commit

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+import { User } from '../user';
+import { AngularFirestore } from '@angular/fire/firestore';
+import  firebase from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  afAuth: any;
+
+  user:any;
+  userInfo:User
+  constructor(private db :AngularFirestore) { }
+
+  signUpUser(user){
+
+    let message = ""
+    firebase.auth().createUserWithEmailAndPassword(user.email, user.password).catch((error) =>{
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      message = errorMessage
+      console.log(errorMessage);
+
+    }).then( results =>{
+      console.log(results);
+      if(results){
+        message = "successfully registered"
+        firebase.database().ref('users/' + results.user.uid).set({
+          firstName: user.firstName,
+          email: user.email,
+          lastName:user.lastName,
+          password:user.password
+          
+        });
+        console.log(message);
+      
+      }else{
+       
+      }
+      
+    });
+    
+  }
+signInUser(email,password){
+  let user :any
+  let message = ""
+  firebase.auth().signInWithEmailAndPassword(email, password).catch((error) =>{
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    message = errorMessage
+    console.log(errorMessage);
+    // ...
+  }).then(result =>{
+    user = result
+
+
+    if(user){
+      message = user.user.email + " has successfully logged in"
+      console.log(message);
+    }else{
+      
+      console.log(message);
+    }
+    
+    return user.user.email
+  });
+ 
+}
+resetPassword(email: string) {
+  const fbAuth = firebase.auth();
+
+  return fbAuth.sendPasswordResetEmail(email)
+    .then(() => console.log('sent Password Reset Email!'))
+    .catch((error) => console.log(error))
+}
+getCurrentUser(){
+   
+  firebase.auth().onAuthStateChanged((user) =>{
+    if (user) {
+      var userId = user.uid;
+     firebase.database().ref('/users/' + userId).once('value').then( userProfile =>{
+        this.userInfo = new User (userProfile.val().firstName, userProfile.val().lastName, userProfile.val().email)
+        console.log(this.userInfo);
+        // return userInfo
+      })
+    } else {
+      console.log("user not logged in");
+      
+    }
+  });
+
+  
+  
+
+}
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  user:User
+  currentUser
+  public contactForm: FormGroup;
 
-  constructor() { }
+  constructor(public authService: AuthService, private route: Router,private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.contactForm = this._formBuilder.group({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: ""
+    
+
+    });
+  }
+  signUp() {
+
+
+    // this.user = new User(this.firstName, this.lastName, this.email, this.password);
+    this.authService.signUpUser(this.contactForm.value);
+    this.route.navigate(['']);
+    this.authService.getCurrentUser()
+  }
   }
 
-}
+
